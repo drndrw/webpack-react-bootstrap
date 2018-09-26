99e157bafac15a298bb78f4d31de3962da379e7d
Andrew DaRe <andrew@duh.re>
2018-09-26 00:37:06 -0700

    Modified rules for react

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,15 @@ module.exports = {
     })
   ],
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }
+  ]
+},
+resolve: {
+  extensions: ['*', '.js', '.jsx']
+},
 }
