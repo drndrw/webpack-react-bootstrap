c559351560bb34fa5c38a294a4160116fe1469b1
Andrew DaRe <andrew@duh.re>
2018-09-27 21:40:58 -0700

    Installed basic React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-const Navigo = require('navigo');
-const root = null;
-const useHash = true;
-const hash = '#';
-const router = new Navigo(root, useHash, hash);
 const anchor = document.getElementById('app');
 import React from "react";
 import ReactDOM from "react-dom";
+import BrowserRouter from "react-router-dom";
 
 const Navigation = () => {
   return <ul>
@@ -14,37 +10,15 @@ const Navigation = () => {
         </ul>;
 };
 
-router.on(() => {
-  document.title = 'Index | DaRe';
-  ReactDOM.render(<h2>Welcome to DaRe page</h2>, anchor);
-});
+const App () => {
+  return <div>
+          <Navigation />
+          <h1>Sup bois</h1>
+        </div>;
+};
 
-router.on({
-  'about': () => {
-    document.title = 'About | DaRe';
-    ReactDOM.render(<h2>Das about page</h2>, anchor);
-  },
-  'portfolio': () => {
-    document.title = 'Portfolio | DaRe';
-    ReactDOM.render(<div>Portfolio</div>, anchor);
-  },
-  'react': () => {
-    document.title = 'React | DaRe';
-    const Index = () => {
-      return <div>
-              <Navigation />
-              <h1>Hey dude</h1>
-              <p>Welcome to my sick react page</p>
-            </div>;
-    };
-    ReactDOM.render(<Index />, anchor);
-  }
-});
-
-router.notFound((query) => {
-  anchor.innerHTML = '<h1>404</h1>';
-  document.title = '404 | DaRe';
-  document.body.appendChild(anchor);
-});
-
-router.resolve();
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), anchor)
