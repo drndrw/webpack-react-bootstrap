f164b44a092d691c8615d3e7b59d9c6887cfca2b
Andrew DaRe <andrew@duh.re>
2018-09-28 18:45:32 -0700

    Working react router config behind Nginx

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 const anchor = document.getElementById('app');
 import React from "react";
 import ReactDOM from "react-dom";
-import BrowserRouter from "react-router-dom";
+import {Switch, Route, BrowserRouter, browserHistory, Link} from "react-router-dom";
 
-const Navigation = () => {
-  return <ul>
-          <li>Home</li>
-          <li>About</li>
-        </ul>;
-};
+const Navigation = () => (
+  <ul>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+        </ul>
+);
 
-const App = () => {
-  return <div>
+const App = () => (
+  <div>
           <Navigation />
-          <h1>Sup bois</h1>
-        </div>;
-};
+          <Main />
+        </div>
+);
+
+const Home = () => (
+  <h1>Sup broh. Welcome to my page</h1>
+);
+
+const About = () => (
+  <h1>About me!!</h1>
+);
+
+const NotFound = () => (
+  <h1>404 - not found...</h1>
+);
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route component={NotFound} />
+    </Switch>
+  </main>);
+
 
 ReactDOM.render((
   <BrowserRouter>
