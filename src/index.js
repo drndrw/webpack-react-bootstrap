3b4ad33acb7be007965e49f13bd78ea453a0e769
Andrew DaRe <andrew@duh.re>
2018-09-30 21:46:57 -0700

    Compile Bootstrap into index.html

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const anchor = document.getElementById('app');
 import React from "react";
 import ReactDOM from "react-dom";
 import {Switch, Route, BrowserRouter, browserHistory, Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navigation = () => (
   <ul>
--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,38 @@ module.exports = {
     })
   ],
   module: {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    }
-  ]
-},
-resolve: {
-  extensions: ['*', '.js', '.jsx']
-},
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+      // {
+      //   test: /\.(scss)$/,
+      //   use: [{
+      //     loader: 'style-loader', // inject CSS to page
+      //   }, {
+      //     loader: 'css-loader', // translates CSS into CommonJS modules
+      //   }, {
+      //     loader: 'postcss-loader', // Run post css actions
+      //     options: {
+      //       plugins: function () { // post css plugins, can be exported to postcss.config.js
+      //         return [
+      //           require('precss'),
+      //           require('autoprefixer')
+      //         ];
+      //       }
+      //     }
+      //   }, {
+      //     loader: 'sass-loader' // compiles Sass to CSS
+      //   }]
+      // },
+    ]
+  }
 }
