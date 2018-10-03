3b13cad9d06ee4e95082385083eab3c50d96d00e
Andrew DaRe <andrew@duh.re>
2018-10-02 19:57:17 -0700

    Removed extract-text-webpack-plugin from webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: ['./src/index.js', './src/scss/app.scss'],
@@ -16,20 +15,12 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.ejs'
-    }),
-    new ExtractTextPlugin('style.css')
+    })
   ],
 }
